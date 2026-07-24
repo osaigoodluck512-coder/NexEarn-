32280ff3078efa0d4c3b2b911cbe89156575d985
osaigoodluck512-coder <osaigoodluck512@gmail.com>
2026-07-24 22:05:28 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,21 @@
-u
+const mongoose = require("mongoose");
+
+const connectDatabase = async () => {
+
+    try{
+
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("MongoDB Connected Successfully");
+
+    }catch(error){
+
+        console.log(error.message);
+
+        process.exit(1);
+
+    }
+
+};
+
+module.exports = connectDatabase;
