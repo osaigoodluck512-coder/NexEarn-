243ba9a3813ee49c1d2193656f62c20071a7b343
osaigoodluck512-coder <osaigoodluck512@gmail.com>
2026-07-24 21:54:31 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-y
+// ==============================
+// NEXEARN BACKEND
+// ==============================
+
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+        success: true,
+        project: "NexEarn Backend",
+        status: "Online",
+        version: "1.0.0"
+    }));
+
+});
+
+server.listen(PORT, () => {
+    console.log(`NexEarn Backend running on port ${PORT}`);
+});
