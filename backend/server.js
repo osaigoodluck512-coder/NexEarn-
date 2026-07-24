79a59e3f691805924cfc1c5e3d13338075fb9aca
osaigoodluck512-coder <osaigoodluck512@gmail.com>
2026-07-24 22:10:55 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
 
