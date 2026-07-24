a5c4e3cc2a1f21517604f9d96135829b8b3129ae
osaigoodluck512-coder <osaigoodluck512@gmail.com>
2026-07-24 22:15:40 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    register
+
+    register,
+
+    login
+
 } = require("../controllers/authController");
 
 router.post("/register", register);
+
+router.post("/login", login);
 
 module.exports = router;
